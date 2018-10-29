e1dd8ae1ed4a32a508626b2f213ad117e5cd19aa
1805783671 <15156350313163.com>
2018-10-29 17:07:41 +0800

    包的命名规范com.happy.buy.模块名.mapper/pojo/interface等等.

---
+++ buy_manager/buy_manager_pojo/src/main/java/com/happy/buy/manager/pojo/po/Entity.java
@@ -0,0 +1,16 @@
+package com.happy.buy.manager.pojo.po;
+
+import java.io.Serializable;
+
+/**
+ * @BelongsProject: happy_shopping
+ * @BelongsPackage: com.happy.buy.manager.pojo.po
+ * @Author: 1805783671
+ * @CreateTime: 2018-10-29 17:04
+ * @Description: 实体类
+ */
+public class Entity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    //....包的命名规则
+}
